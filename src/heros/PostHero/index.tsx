a1f4737f20dbd0ff01cef5014cedc60303d87cd3
Luk Lambrechts <luk@lenoweb.be>
2025-08-19 15:10:34 +0200

    reduce height of the navigational menu

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -15,7 +15,7 @@ export const PostHero: React.FC<{
     populatedAuthors && populatedAuthors.length > 0 && formatAuthors(populatedAuthors) !== ''
 
   return (
-    <div className="relative flex items-end">
+    <div className="relative -mt-8 flex items-end">
       <div className="container z-10 relative lg:grid lg:grid-cols-[1fr_48rem_1fr] text-white pb-8">
         <div className="col-start-1 col-span-1 md:col-start-2 md:col-span-2">
           <div className="uppercase text-sm mb-6">
