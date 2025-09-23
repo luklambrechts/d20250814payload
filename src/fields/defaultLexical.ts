c6e9ff058d50bc3324f3fe00cdd3a12d0b17ae06
luklambrechts <lambrechts_luk@hotmail.com>
2025-09-23 14:45:11 +0200

    problem with links

--- src/fields/defaultLexical.ts
+++ src/fields/defaultLexical.ts
@@ -20,17 +20,6 @@ export const defaultLexical = lexicalEditor({
     HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }),
     LinkFeature({
       enabledCollections: ['pages', 'posts'],
-      fields: ({ defaultFields }) => {
-        return defaultFields.map((field) => {
-          if (field.name === 'doc') {
-            return {
-              ...field,
-              required: false, // Make doc field optional to prevent validation errors
-            }
-          }
-          return field
-        })
-      },
     }),
     UploadFeature({
       collections: {
